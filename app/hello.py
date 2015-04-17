8f80331ce72f7d268d1840c16e7b4397b07522a3
Pedro Baumann <ondoheer@gmail.com>
2015-04-17 17:34:33 -0500

    m

---
+++ app/hello.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/<string:name>")
+def name(name):
+    return render_template("saludo.html", name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
